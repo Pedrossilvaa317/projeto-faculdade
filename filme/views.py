c5c5f73176a8dbf2aa3bee2ecb2e253d578fcc38
Pedro Coelho <pedrocsilva2004@gmail.com>
2025-09-12 14:16:46 -0300

    mudanças da aula 08/09

--- filme/views.py
+++ filme/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Filmes
+from .forms import FilmesForm
 
 def list_filmes(request):
     filmes = Filmes.objects.all()
@@ -8,3 +9,18 @@ def list_filmes(request):
         'filmes' : filmes
     }
     return render (request, template_name,context)
+
+def new_filmes(request):
+    print ('new_filmes', request.method)
+    if request.method == 'POST':
+        form = FilmesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('filme:list_filmes')
+    else:
+        template_name = 'new_filmes.html'
+        context = {
+            'mensagem':'Cadastro de filmes',
+            'form':FilmesForm()
+        }
+        return render(request, template_name, context)
