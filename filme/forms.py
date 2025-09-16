b8c1866283ce2906f7080a7445b55e109039c045
Pedro Coelho <pedrocsilva2004@gmail.com>
2025-09-16 00:14:19 -0300

    Delete filme directory

--- filme/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Filmes
-
-class FilmesForm(ModelForm): #usar palavras arbitratios (o nome da class junto com forms)
-    class Meta:
-        model = Filmes
-        fields = '__all__'
